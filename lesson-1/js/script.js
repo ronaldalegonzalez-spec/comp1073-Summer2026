35056a2f2b1445f601d9b442b440540f16f1f27a
RonaldGon <ronaldalegonzalez@gmail.com>
2026-05-06 10:26:28 -0400

    Completed lesson 1

--- lesson-1/js/script.js
+++ lesson-1/js/script.js
@@ -1,4 +1,4 @@
-		/*
+	/*
 		STEP 1: What is JavaScript?
 
 		JavaScript is an interpreted language, rather than a compiled language like C/C++. That is - the code is run in a linear way, then the result is returned as output of some kind.
@@ -22,17 +22,19 @@
 		Browser APIs allow JavaScript to do things like update the Document Object Model (DOM API), locate the user (Geolocation API), generate 2D and 3D graphics (Canvas and WebGL APIs), and work with audio and video (Multimedia APIs).
 		*/
 
-		// STEP 2: Get the EM element and store as a variable, using const
-		
+		// STEP 2: Get the EM element and store as a variable, using const.
+		const element = document.querySelector("em");
 		// STEP 3: Assign a click event listener to the above element, and include a reference to the function to be invoked
-		
+		element.addEventListener("click", updateName);
 		// STEP 4: Creat a function that waits for the above click event
-		
+		function updateName(){
+
 			// STEP 4a: Display a prompt asking for input, and assign the input to a variable using let
-			
+			let firstName = prompt("What's your name?");
+			console.log(firstName);
 			// STEP 4b: Grab the input from the variable and build a new string, then change the text inside the EM element, using the textContent property
-			
-		
+			element.textContent = firstName;
+		}
 		// STEP 5: Comment out the addEventListener above, and replace it with an onclick attribute typed directly into the EM element in the HTML - note that this is common, but it is considered to be bad practice - separate your HTML and your JS
 
 		// STEP 6: Move the above JavaScript into an external file and reference it with an empty SCRIPT element inside the HEAD element - be sure to add a defer attribute in the SCRIPT start tag
