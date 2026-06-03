012d56b5e0cbc8c11737e6d336a718e3b2e9a098
RonaldGon <ronaldalegonzalez@gmail.com>
2026-06-03 11:00:40 -0400

    Lesson 5 completed

--- lesson-5/js/1-intro.js
+++ lesson-5/js/1-intro.js
@@ -1,12 +1,15 @@
-// STEP 1: Build the main entry point for the program
-
+async function startProgram() {
+	// STEP 1: Build the main entry point for the program
+async function startProgram(){
     // STEP 2: Call a method to control the main LED lights
-    
+    setMainLed(getRandomColor());
     // STEP 3: Invoke another method to write to the screen
-    
+    await scrollMatrixText("Hello World!", getRandomColor(), 24, false);
     // STEP 3: Call another method to move the robot
-    
+    await roll(0, 30, 2);
     // STEP 4: Exit the program
-    
-
+    exitProgram();
+}
 // STEP 5: Aim your robot, then run the program
+
+}
