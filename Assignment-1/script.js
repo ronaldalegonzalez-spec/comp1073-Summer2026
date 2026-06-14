d6ac16f7713d25dafde9f372f8477b164eb87618
RonaldGon <ronaldalegonzalez@gmail.com>
2026-06-14 19:01:15 -0400

    first btn connection

---
+++ Assignment-1/script.js
@@ -0,0 +1,6 @@
+const subjectBtn = document.querySelector('#subject-btn');
+const page = document.querySelector('body');
+
+subjectBtn.addEventListener("click", function() {
+				page.style.backgroundColor = "red";
+			});
