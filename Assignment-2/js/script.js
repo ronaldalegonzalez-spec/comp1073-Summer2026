8927b881092683980a3291c189c62ad82f572d7a
RonaldGon <ronaldalegonzalez@gmail.com>
2026-07-27 21:08:36 -0400

    Plan for the js in steps

---
+++ Assignment-2/js/script.js
@@ -0,0 +1,6 @@
+//Step 1: listen "click" event
+//Step 2: Prevent default
+//Step 3: Read info from the inputs (read user info/ values)
+//Step 4: Validation
+//Step 5: Create an Object with the user info (need to use a class, constructor, and a method inside the Object)
+//Step 6: Generate my name and student number with JavaScript (footer)
