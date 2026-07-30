d178a040d583427886ed4a1357dee17c747581f4
RonaldGon <ronaldalegonzalez@gmail.com>
2026-07-29 20:13:41 -0400

    First half of the class completed

--- lesson-11/js/battery.js
+++ lesson-11/js/battery.js
@@ -12,20 +12,35 @@ const chargeMeter = document.querySelector(
 /* Functions
 -------------------------------------------------- */
 // STEP 3a: Create the updateBatteryStatus() function
-
+function updateBatteryStatus(battery){
 // STEP 3b: Update the charging status
-
+    if (battery.charging){
+        chargeStatus.textContent = "Charging...";
+} else {
+    chargeStatus.textContent = "Discharging...";
+}
 // STEP 3c: Update the charge level
+chargeLevel.textContent = (battery.level * 100 + "%");
+chargeMeter.value = battery.level * 100;
+}
 
 // STEP 2a: Using the getBattery() method of the navigator object,
 //create a promise to retrieve the battery information
-
+navigator.getBattery().then((battery) => {
+    
 // STEP 2b: See what the battery object contains
+console.log(battery);
 
 // STEP 3d: Update the battery information when the promise resolves
+updateBatteryStatus(battery);
 
 // STEP 4a: Event listener for changes to the charging status
-
+battery.addEventListener("chargingchange", function () {
+        updateBatteryStatus(battery);
+});
 // STEP 4b: Event listener for changes to the charge level
-
+battery.addEventListener("levelchange", function () {
+        updateBatteryStatus(battery);
+});
+});
 /* This script adapted from the excellent code examples found at https://www.w3.org/TR/battery-status/#examples and https://developer.mozilla.org/en-US/docs/Web/API/Battery_Status_API */
--- lesson-11/js/nytimes.js
+++ lesson-11/js/nytimes.js
@@ -1,7 +1,7 @@
 // The URL for the Article Search API at nytimes.com
 const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 // STEP 1: Get your own API key and paste it below…
-const key = "";
+const key = "kVsdvkXM6yEiaUwdQA6gZvXRRWyjcbkAaAFP1X5IOpdJ17zf";
 let url;
 // Grab references to all the DOM elements you'll need to access
 const searchTerm = document.querySelector(".search");
@@ -11,24 +11,37 @@ const searchForm = document.querySelector("form");
 const submitBtn = document.querySelector(".submit");
 const section = document.querySelector("section");
 // STEP 2: Add a submit event listener for the search form, referencing the fetchResults function as the callback
+submitBtn.addEventListener("click", function (event) {
+    fetchResults(event);
+});
 
 // Functions
 function fetchResults(event) {
     // Use preventDefault() to stop the form submitting
+    event.preventDefault();
     // STEP 3: Assemble the full URL, according to the API documentation at the New York Times
+    url = `${baseURL}?q=${searchTerm.value}&api-key=${key}`;
+    console.log(url);
+
+    // Exercise: Add start date and end date to the api url
+
     // STEP 4: Use fetch() to pass the URL that we built as a request to the API service, then pass the JSON to the displayResults() function
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((json) => displayResults(json))
 }
 
 function displayResults(json) {
     // STEP 5: Log to the console the results from the API
-
+    console.log(json);
     // Clear out the old results…
     while (section.firstChild) {
         section.removeChild(section.firstChild);
     }
     // STEP 6: Create the variable articles to capture the articles from the JSON object
+    let articles = json.response.docs;
 
-    if (articles.length === 0) {
+    if (articles === null) {
         const para = document.createElement("p");
         para.textContent = "No results returned.";
         section.appendChild(para);
