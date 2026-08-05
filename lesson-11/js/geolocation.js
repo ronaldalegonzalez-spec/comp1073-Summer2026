759e51bcbe7902de5608d68a2648044f8e5aad07
RonaldGon <ronaldalegonzalez@gmail.com>
2026-08-05 19:08:32 -0400

    Completed lesson-11

--- lesson-11/js/geolocation.js
+++ lesson-11/js/geolocation.js
@@ -6,33 +6,45 @@ const latitude = document.querySelector("#latlong dd:nth-of-type(1)");
 const longitude = document.querySelector("#latlong dd:nth-of-type(2)");
 // STEP 1c: Grab the <p> element for outputting geolocation status messages
 const statusMsg = document.querySelector("#status");
-// STEP 1d: Grap the <a> element to use as a link to OpenMaps if the geolocation was successful
+// STEP 1d: Grab the <a> element to use as a link to OpenMaps if the geolocation was successful
 const mapLink = document.querySelector("#mapLink");
 
 /* Functions
 -------------------------------------------------- */
 // STEP 3b: Build out the success() function, receiving the position as a parameter
+function success(position) {
+    statusMsg.textContent = "We found you...";
+    console.log(position);
 
-// STEP 3c: Output the latitude and longitude coordinates to the <dd> elements in steps 1a and 1b
-
-// STEP 3d: Build out the link to OpenStreetMap
+    // STEP 3c: Output the latitude and longitude coordinates to the <dd> elements in steps 1a and 1b
+    latitude.textContent = position.coords.latitude + "°";
+    longitude.textContent = position.coords.longitude + "°";
+    
+    // STEP 3d: Build out the link to OpenStreetMap
+    let url = `https://www.openstreetmap.org/#map=18/${position.coords.latitude}/${position.coords.longitude}`;
+    mapLink.setAttribute("href", url);
+    mapLink.setAttribute("target", "_blank");
+    mapLink.textContent = "Open on maps";
+}
 
 // STEP 4a: Construct the error() function
-
-// STEP 4b: Output a suitable error message
-
+function error() {
+    // STEP 4b: Output a suitable error message
+    statusMsg.textContent = "Sorry, we can not find you.";
+}
 /* Script Logic
 -------------------------------------------------- */
 // STEP 2a: Check support (the use will be asked for permission to allow for geolocation for security purposes, which is a good thing)
-
-// STEP 2b: Geolocation is not supported, so output useful message
-
-// STEP 2c: Geolocation is supported, so let's give the user a useful message
-
-// STEP 2d: Let's have a look at the geolocation object
-
-// STEP 3a: Use the getCurrentPosition() method, which passes the device position to a named callback function (if successful), or it calls an error function if it fails
-
-// STEP 5: Try out the script on your mobile device - be sure to walk somewhere else in your office or classroom, then refresh the page to see your position change
-
+if (!navigator.geolocation) {
+    // STEP 2b: Geolocation is not supported, so output useful message
+    statusMsg.textContent = "Geolocation is not supported by your browser.";
+    // STEP 2c: Geolocation is supported, so let's give the user a useful message
+} else {
+    // STEP 2d: Let's have a look at the geolocation object
+    statusMsg.textContent = "loading...";
+    // STEP 3a: Use the getCurrentPosition() method, which passes the device position to a named callback function (if successful), or it calls an error function if it fails
+    console.log(navigator.geolocation);
+    // STEP 5: Try out the script on your mobile device - be sure to walk somewhere else in your office or classroom, then refresh the page to see your position change
+    navigator.geolocation.getCurrentPosition(success, error);
+}
 /* This script adapted from the excellent code examples found at https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API#examples with a tip of the hat to https://www.openstreetmap.org/ */
--- lesson-11/js/orientation.js
+++ lesson-11/js/orientation.js
@@ -22,24 +22,29 @@ const statusMsg = document.querySelector("#status");
 
 // STEP 4b: Output a suitable error message
 
+
 /* Script Logic
 -------------------------------------------------- */
 // STEP 2a: Check support for the DeviceOrientationEvent
-
-// STEP 2b: Do something helpful if there is no support for the DeviceOrientationEvent
-
-// STEP 3a: Build an event listener for the 'deviceorientation' event, and build out an anonymous callback function, passing in the event itself
-
-// STEP 3b: Capture the alpha value (rotation) and set the textContent for the <output> element
-
-// STEP 3c: Use the same value to update the position of the slider
-
-// STEP 3d: Capture the beta value (tilt toward/away) and set the textContent for the <output> element
-
-// STEP 3e: Use the same value to update the position of the slider
-
-// STEP 3f: Capture the gamma value (tilt left/right) and set the textContent for the <output> element
-
-// STEP 3g: Use the same value to update the position of the slider
-
+if (!window.DeviceOrientationEvent) {
+    statusMsg.textContent = "Sorry, Device OrientationEvent is not supported on this device/browser.";
+    // STEP 2b: Do something helpful if there is no support for the DeviceOrientationEvent
+} else {
+    // STEP 3a: Build an event listener for the 'deviceorientation' event, and build out an anonymous callback function, passing in the event itself
+    window.addEventListener('deviceorientation', function (event) {
+        console.log(event);
+        // STEP 3b: Capture the alpha value (rotation) and set the textContent for the <output> element
+        alphaValue.textContent = Math.round(event.alpha) + "°";
+        // STEP 3c: Use the same value to update the position of the slider
+        alphaSlider.value = Math.round(event.alpha);
+        // STEP 3d: Capture the beta value (tilt toward/away) and set the textContent for the <output> element
+        betaValue.textContent = Math.round(event.beta) + "°";
+        // STEP 3e: Use the same value to update the position of the slider
+        betaSlider.value = Math.round(event.beta);
+        // STEP 3f: Capture the gamma value (tilt left/right) and set the textContent for the <output> element
+        gammaValue.textContent = Math.round(event.gamma) + "°";
+        // STEP 3g: Use the same value to update the position of the slider
+        gammaSlider.value = Math.round(event.gamma);
+    });
+}
 /* Learn more at https://developer.mozilla.org/en-US/docs/Web/API/DeviceOrientationEvent */
